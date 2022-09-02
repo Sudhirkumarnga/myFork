69d4b11da8491d4e8ebfa3ddd23b0b9a2a07041a
milyasyousuf <m_ilyas@outlook.com>
2022-09-02 18:56:51 +0500

    Bug fixed

--- backend/home/services.py
+++ backend/home/services.py
@@ -15,10 +15,11 @@ from business.models import (
     EmergencyContact
 )
 
+
 def create_business_and_business_address(user, data):
     business = Business.objects.create(
         user=user,
-        employe_types = data['employee_types'],
+        employe_types=data['employee_types'],
         business_code=get_random_string(7)
     )
     business_address = BusinessAddress.objects.create(
@@ -26,45 +27,51 @@ def create_business_and_business_address(user, data):
     )
     return business
 
-def create_employee(business,user,data):
+
+def create_employee(business, user, data):
     Employee.objects.create(
-        #first_name = data.get("first_name", ""),
-        #phone = data.get("phone", ""),
+        # first_name = data.get("first_name", ""),
+        # phone = data.get("phone", ""),
         user=user,
         business=business,
         is_owner=True
     )
 
-def create_organization_employee(user,data):
+
+def create_organization_employee(user, data):
     employee = Employee.objects.create(
         user=user,
         business=Business.objects.get(
             business_code=data['business_code']
         ),
-        mobile= data['phone'],
-        first_name=data['first_name'],
+        mobile=data['phone'],
         is_owner=False
     )
     return employee
+
 
 def create_emergency_contact(employee):
     EmergencyContact.objects.create(
         employee=employee
     )
 
+
 def generate_user_otp(user):
     otp = format(random.randint(0, 9999), '04d')
     user_otp = User_OTP.objects.filter(user=user)
     if user_otp.exists():
-        user_otp.update(otp=otp,is_expire=False)
+        user_otp.update(otp=otp, is_expire=False)
     else:
         user_otp = User_OTP.objects.create(user=user, otp=otp)
 
     return otp
 
-def send_account_confirmation_email(user,otp):
-    msg_plain = render_to_string('email/account_confirmation_email.txt', {'user':user ,'otp': otp,'domain':current_site.domain})
-    msg_html = render_to_string('email/account_confirmation_email.html', {'user':user ,'otp': otp,'domain':current_site.domain})
+
+def send_account_confirmation_email(user, otp):
+    msg_plain = render_to_string('email/account_confirmation_email.txt',
+                                 {'user': user, 'otp': otp, 'domain': current_site.domain})
+    msg_html = render_to_string('email/account_confirmation_email.html',
+                                {'user': user, 'otp': otp, 'domain': current_site.domain})
     send_mail(
         'Account Confirmation Email',
         msg_plain,
@@ -73,9 +80,12 @@ def send_account_confirmation_email(user,otp):
         html_message=msg_html,
     )
 
-def sent_password_reset_email(user,otp):
-    msg_plain = render_to_string('email/password_reset_email.txt', {'user':user ,'otp': otp,'domain':current_site.domain})
-    msg_html = render_to_string('email/password_reset_email.html', {'user':user ,'otp': otp,'domain':current_site.domain})
+
+def sent_password_reset_email(user, otp):
+    msg_plain = render_to_string('email/password_reset_email.txt',
+                                 {'user': user, 'otp': otp, 'domain': current_site.domain})
+    msg_html = render_to_string('email/password_reset_email.html',
+                                {'user': user, 'otp': otp, 'domain': current_site.domain})
 
     send_mail(
         'Password Reset Email',
@@ -85,9 +95,10 @@ def sent_password_reset_email(user,otp):
         html_message=msg_html,
     )
 
-def send_new_otp(user,otp):
-    msg_plain = render_to_string('email/otp_email.txt', {'user':user ,'otp': otp,'domain':current_site.domain})
-    msg_html = render_to_string('email/otp_email.html', {'user':user ,'otp': otp,'domain':current_site.domain})
+
+def send_new_otp(user, otp):
+    msg_plain = render_to_string('email/otp_email.txt', {'user': user, 'otp': otp, 'domain': current_site.domain})
+    msg_html = render_to_string('email/otp_email.html', {'user': user, 'otp': otp, 'domain': current_site.domain})
 
     send_mail(
         'Password Reset Email',
