2aa8ef2bcb224ff85912985bad16a8f63d5ee4f4
milyasyousuf <m_ilyas@outlook.com>
2022-11-13 17:38:46 +0500

    Bug fixed in Updation of Worksite

--- backend/workside/services.py
+++ backend/workside/services.py
@@ -42,12 +42,17 @@ def update_worksite(user, data, instance):
         business=Business.objects.get(user=user)
     )
     worksite = models.WorkSite.objects.get(id=instance.id)
-    if 'show_dtails' in data:
+    if data.__contains__("show_dtails"):
         worksite.show_dtails = data['show_dtails']
-    if 'logo' in data:
+    if data.__contains__("logo") and data['logo'] is not None:
         worksite.logo = convert_file_from_bse64_to_blob(data['logo'])
-    if 'instruction_video' in data:
+    if data.__contains__("instruction_video") in data and data['instruction_video'] is not None:
         worksite.instruction_video = convert_file_from_bse64_to_blob(data['instruction_video'])
+    if data.__contains__("logo") and data['logo'] is None:
+        worksite.logo = data['logo']
+    if data.__contains__("instruction_video") in data and data['instruction_video'] is None:
+        worksite.instruction_video = data['instruction_video']
+
     worksite.save()
 
 
