2372bab8f6c4b10110e4b1b91027d6317ba9b5af
milyasyousuf <m_ilyas@outlook.com>
2022-12-21 20:03:35 +0500

    FCM Ssettings added

--- backend/smart_workhorse_33965/settings.py
+++ backend/smart_workhorse_33965/settings.py
@@ -10,19 +10,14 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import json
 import os
-import io
 import environ
 import logging
-import google.auth
-from google.cloud import secretmanager
-from google.auth.exceptions import DefaultCredentialsError
-from google.api_core.exceptions import PermissionDenied
 from modules.manifest import get_modules
+from pathlib import Path
 import firebase_admin
 from firebase_admin import credentials
-from pathlib import Path
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -345,18 +340,32 @@ DJSTRIPE_WEBHOOK_SECRET = "whsec_48e8a215af1fa02eda2299b82a9978a8438a11415b604a8
 DJSTRIPE_FOREIGN_KEY_TO_FIELD = "id"
 
 
+
+
+# FCM_DJANGO_SETTINGS = {
+#     # default: _('FCM Django')
+#     "APP_VERBOSE_NAME": "Smart Work Horse",
+#     # Your firebase API KEY
+#     "FCM_SERVER_KEY": env.str("FCM_SERVER_KEY", "FCM_SERVER_KEY"),
+#     # true if you want to have only one active device per registered user at a time
+#     # default: False
+#     "ONE_DEVICE_PER_USER": False,
+#     # devices to which notifications cannot be sent,
+#     # are deleted upon receiving error response from FCM
+#     # default: False
+#     "DELETE_INACTIVE_DEVICES": False,
+# }
+
+
+FCM_DJANGO_SETTINGS = {"FCM_SERVER_KEY": env("FCM_SERVER_KEY", default=None)}
+cred = credentials.Certificate(env.json("FIREBASE_CREDENTAILS", default=None))
+firebase_admin.initialize_app(cred)
+
 FCM_DJANGO_SETTINGS = {
-    # default: _('FCM Django')
     "APP_VERBOSE_NAME": "Smart Work Horse",
-    # Your firebase API KEY
-    "FCM_SERVER_KEY": env.str("FCM_SERVER_KEY", ""),
-    # true if you want to have only one active device per registered user at a time
-    # default: False
     "ONE_DEVICE_PER_USER": False,
-    # devices to which notifications cannot be sent,
-    # are deleted upon receiving error response from FCM
-    # default: False
     "DELETE_INACTIVE_DEVICES": False,
 }
+
 
 OLD_PASSWORD_FIELD_ENABLED = True
