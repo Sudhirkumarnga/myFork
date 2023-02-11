d72877e21e630eea36bac2a4fae2fffc47a9c49d
milyasyousuf <m_ilyas@outlook.com>
2023-02-11 12:57:22 +0500

    Bug fixed

--- backend/business/admin.py
+++ backend/business/admin.py
@@ -6,8 +6,28 @@ from business.models import Business, Employee, BusinessAddress, Attendance, Lea
 admin.site.register(Business)
 admin.site.register(Employee)
 admin.site.register(BusinessAddress)
-admin.site.register(Attendance)
 admin.site.register(LeaveRequest)
 admin.site.register(EmergencyContact)
 admin.site.register(Feedback)
 admin.site.register(FeedbackMedia)
+
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_filter = ["clock_in_time", "clock_out_time", "status", "is_shift_completed"]
+    search_fields = [
+        "employee__user__email",
+        "worksite_name",
+        "event"
+    ]
+    list_display = [
+        "employee", 
+        "hourly_rate", 
+        "event", 
+        "worksite_name", 
+        "clock_in_time", 
+        "clock_out_time", 
+        "total_hours", 
+        "earnings", 
+        "is_shift_completed"
+    ]
